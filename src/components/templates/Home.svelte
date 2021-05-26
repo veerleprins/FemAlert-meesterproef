edbc3a5eb07523f755974d98fa16ce89a9e19f75
johancvb <35685184+johancvb@users.noreply.github.com>
2021-05-26 15:37:11 +0200

    Fixed issue

--- src/components/templates/Home.svelte
+++ src/components/templates/Home.svelte
@@ -1,6 +1,16 @@
 <script>
   // Components
   import Button from '../atoms/Button.svelte'
+
+  import { onMount } from 'svelte'
+
+  let data = []
+
+  onMount(async () => {
+    const res = await fetch(`https://my.api.mockaroo.com/fem_alert_v5?key=6cbcdbc0`)
+    data = await res.json()
+    // o
+  })
 </script>
 
 <style lang="scss">
@@ -14,4 +24,11 @@
 <section>
   <h1>FemAlert</h1>
   <Button URL="">Send</Button>
+</section>
+
+<section>
+  <h1>Data:</h1>
+  {#each data as item}
+  <p>{item.first_name}</p>
+  {/each}
 </section>
