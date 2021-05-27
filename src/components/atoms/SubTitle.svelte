86a55107ca12431c7b4593085cd90dc93680ba20
veerleprins <veerleprins@hotmail.com>
2021-05-27 10:30:50 +0200

    Added two components & new color

---
+++ src/components/atoms/SubTitle.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<style lang="scss">
+  // Import fonts, vars, etc.
+  @import 'src/styles/index.scss';
+  h2 {
+    font-size: $h1-size;
+    color: $ui-black;
+  }
+</style>
+
+<h2>
+  <slot />
+</h2>
