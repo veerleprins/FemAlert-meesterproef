9598351302c75c12a3664420980bb4e46f2b0b4b
veerleprins <veerleprins@hotmail.com>
2021-05-27 14:44:28 +0200

    Created axes components

---
+++ src/components/atoms/AxisBottom.svelte
@@ -0,0 +1,18 @@
+<script>
+  // Props
+  export let xScale
+  export let innerHeight
+</script>
+
+<style lang="scss">
+</style>
+
+{#each yScale.ticks() as tickValue}
+  <g key={tickValue} transform={`translate(${yScale(tickValue)}, 0)`}>
+    <line class="lines" y2={innerHeight} />
+    <text y={innerHeight + 3} dy=".71em" style={{ textAnchor: 'middle' }}>
+      <!-- {tickValue} -->
+      <slot />
+    </text>
+  </g>
+{/each}
--- src/components/atoms/AxisLeft.svelte
+++ src/components/atoms/AxisLeft.svelte
@@ -1,11 +1,19 @@
 <script>
   // Props
+  export let yScale
+  export let innerWidth
+  export let tickFormat
 </script>
 
 <style lang="scss">
 </style>
 
-<g>
-  <line />
-  <text />
-</g>
+{#each yScale.ticks() as tickValue}
+  <g key={tickValue} transform={`translate(0, ${yScale(tickValue)})`}>
+    <line class="lines" x2={innerWidth} />
+    <text x={-3} dy=".32em" style={{ textAnchor: 'end' }}>
+      <!-- {tickFormat(tickValue)} -->
+      <slot />
+    </text>
+  </g>
+{/each}
