b68a25fe2857bd601b9e4096d50470fd3bc2f7d4
veerleprins <veerleprins@hotmail.com>
2021-05-27 16:06:44 +0200

    Created Label component

---
+++ src/components/atoms/Label.svelte
@@ -0,0 +1,14 @@
+<script>
+  // Props
+  export let className
+  export let yVal
+  export let xVal
+  export let transform
+</script>
+
+<style lang="scss">
+</style>
+
+<text class={className} y={yVal} x={xVal} {transform} textAnchor="middle">
+  <slot />
+</text>
