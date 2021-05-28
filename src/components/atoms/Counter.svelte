d55240695c6a9431449eb091083393f96e102013
SharonV33 <sharonveldman33@gmail.com>
2021-05-28 15:37:29 +0200

    minor fix in relation to pull request

--- src/components/atoms/Counter.svelte
+++ src/components/atoms/Counter.svelte
@@ -5,13 +5,14 @@
   // Import fonts, vars, etc.
   @import 'src/styles/index.scss';
 
-  h2 {
+  p {
     font-size: 2em;
+    font-weight: bold;
     color: $ui-black;
   }
 
 </style>
 
-<h2>
+<p>
   <slot />
-</h2>
+<p>
