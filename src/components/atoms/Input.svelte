92badd8199d21a9d3d93c82c291bb27771ea7ecc
veerleprins <veerleprins@hotmail.com>
2021-06-04 12:09:43 +0200

    Added styling & class to Input component

--- src/components/atoms/Input.svelte
+++ src/components/atoms/Input.svelte
@@ -2,9 +2,23 @@
   // Props
   export let type
   export let name
+  export let placeholder
+  export let searchBar = false
 </script>
 
 <style lang="scss">
+  // Importing fonts, vars, etc.
+  @import 'src/styles/index.scss';
+
+  input {
+    &.searchBar {
+      width: 100%;
+      border: none;
+      border-radius: $borderSize;
+      height: 40px;
+      padding-left: 20px;
+    }
+  }
 </style>
 
-<input {type} {name} />
+<input {type} {name} {placeholder} class:searchBar />
