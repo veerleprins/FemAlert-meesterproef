a7e9c13275b9beab0df49936b1ab0d38cb48e18b
veerleprins <veerleprins@hotmail.com>
2021-06-07 16:23:15 +0200

    Changed name to dutch for routing

---
+++ src/routes/meldingen.svelte
@@ -0,0 +1,13 @@
+<script>
+  // Components
+  import Reports from '@/components/templates/Reports.svelte'
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+  <title>Alle meldingen | FemAlert</title>
+</svelte:head>
+
+<Reports />
