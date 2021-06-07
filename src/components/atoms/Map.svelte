5a4180b69c261e4579589c7b5fe9e470b25316d6
johancvb <35685184+johancvb@users.noreply.github.com>
2021-06-07 10:46:32 +0200

    Added map atom

--- src/components/atoms/Map.svelte
+++ src/components/atoms/Map.svelte
@@ -1,4 +1,8 @@
 <script>
+  import * as d3 from 'd3'
+  
+  
+  
 </script>
 
 <style>
--- src/components/templates/Map.svelte
+++ src/components/templates/Map.svelte
@@ -1,5 +1,20 @@
 <script>
+  import Title from '../atoms/Title.svelte'
+  import Map from '../atoms/Map.svelte'
 </script>
 
 <style>
 </style>
+
+<header>
+  <Title>Test</Title>
+</header>
+
+<div class="grid-container">
+  <div class="icon" />
+  <div class="welcome" />
+  <div class="nav" />
+  <div class="content">
+    <Map />
+  </div>
+</div>
