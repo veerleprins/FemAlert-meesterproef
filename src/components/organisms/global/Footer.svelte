facb245f93667146d19a101976dd1b1265c357dc
veerleprins <veerleprins@hotmail.com>
2021-06-11 15:49:41 +0200

    Changed Footer & Header component

---
+++ src/components/organisms/global/Footer.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<style lang="scss">
+</style>
+
+<footer>
+  <p>&copy; Sharon Veldman - Johan van Beusekom - Veerle Prins, 2021.</p>
+</footer>
---
+++ src/components/organisms/global/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  // Components
+  import Nav from '@/components/molecules/Nav.svelte'
+  import Sidebar from '../../atoms/Sidebar.svelte'
+  let open = false
+</script>
+
+<style lang="scss">
+</style>
+
+<header>
+  <Sidebar bind:open />
+  <Nav bind:sidebar={open} />
+</header>
