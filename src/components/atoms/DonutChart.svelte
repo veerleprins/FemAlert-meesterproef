06f4613a4c193e1541e148beca3cf0e7efdc0ff8
SharonV33 <sharonveldman33@gmail.com>
2021-06-11 11:37:07 +0200

    implement changes on donut chart data based on pull request feedback

--- src/components/atoms/DonutChart.svelte
+++ src/components/atoms/DonutChart.svelte
@@ -16,77 +16,61 @@
 
      // Create empty objects which will be used to count the
      // number of reports and to build the donut chart
-     let agressie = {
-        'size': 0,
-         'name': 'Agressie',
-         'color': ''
-     }
-     let aanranding = {
-        'size': 0,
-         'name': 'Aanranding',
-         'color': ''
-     }
-     let uitschelden = {
-        'size': 0,
-         'name': 'Uitschelden',
-         'color': ''
-     }
-    let discriminatie = {
-        'size': 0,
-        'name': 'Discriminatie',
-        'color': ''
+    const getObject = (DynamischeNaam) => {
+        return {
+            name: DynamischeNaam,
+            count: 0,
+            color: ''
+        }
     }
-    let racisme = {
-        'size': 0,
-        'name': 'Rasicme',
-        'color': ''
-    }
-    let anders = {
-        'size': 0,
-        'name': 'Anders',
-        'color': ''
-    }
+    const aanrandingObject = getObject('Aanranding')
+    const racismeObject = getObject('Racisme')
+    const uitscheldenObject = getObject('Uitschelden')
+    const agressieObject = getObject('Agressie')
+    const andersObject = getObject('Anders')
+    const discriminatieObject = getObject('Discriminatie')
 
      // For each array of objects containing report types
      // loop through both the outer arrays and the objects
      // and check the type of report, then add to the correct counter
      allReports.forEach((item) => {
+
         item.forEach((accident) => {
             if (accident.type === 'Aanranding') {
-                aanranding.size ++
-                aanranding.color = accident.color
+                aanrandingObject.count ++
+                aanrandingObject.color = accident.color
             }
             else if (accident.type === 'Agressie') {
-                agressie.size ++
-                agressie.color = accident.color
+                agressieObject.count ++
+                agressieObject.color = accident.color
             }
             else if (accident.type === 'Uitschelden') {
-                uitschelden.size ++
-                uitschelden.color = accident.color
+                uitscheldenObject.count ++
+                uitscheldenObject.color = accident.color
             }
             else if (accident.type === 'Discriminatie') {
-                discriminatie.size ++
-                discriminatie.color = accident.color
+                discriminatieObject.count ++
+                discriminatieObject.color = accident.color
             }
             else if (accident.type === 'Racisme') {
-                racisme.size ++
-                racisme.color = accident.color
+                racismeObject.count ++
+                racismeObject.color = accident.color
             }
             else if (accident.type === 'Anders') {
-                anders.size ++
-                anders.color = accident.color
+                andersObject.count ++
+                andersObject.color = accident.color
             }
         })
     })
 
     // Combine all the new objects into an array
     let data = [
-        agressie,
-        aanranding,
-        uitschelden,
-        discriminatie,
-        racisme,
-        anders
+        agressieObject,
+        aanrandingObject,
+        uitscheldenObject,
+        discriminatieObject,
+        racismeObject,
+        andersObject
     ]
 
     /**
@@ -104,7 +88,7 @@
     //create a full circle
     let angle = Math.PI * 2
 
-    let total = data.reduce((total, item) => total + item.size, 0)
+    let total = data.reduce((total, item) => total + item.count, 0)
     let arcs
         let acc = 0
         //map over data and create an arc for each data point
@@ -117,7 +101,7 @@
                 startAngle: acc,
                 //find the end point of the segment and store this in startAngle
                 //this way the next segment will start where the previous one ended
-                endAngle: (acc += (angle * segment.size / total))
+                endAngle: (acc += (angle * segment.count / total))
             }
             return {
                 color: segment.color,
