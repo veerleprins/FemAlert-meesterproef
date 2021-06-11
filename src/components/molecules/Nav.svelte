d87d3582275b97039082d22c40618039d322e2d5
veerleprins <veerleprins@hotmail.com>
2021-06-11 15:49:17 +0200

    Removed styling & added hamburger component

--- src/components/molecules/Nav.svelte
+++ src/components/molecules/Nav.svelte
@@ -1,41 +1,18 @@
 <script>
   // Imports
-  import List from '../atoms/List.svelte'
+  // import List from '../atoms/List.svelte'
+  import Hamburger from '@/components/atoms/Hamburger.svelte'
+
+  export let sidebar = false
 </script>
 
 <style lang="scss">
   @import 'src/styles/index.scss';
   nav {
-    display: none;
-  }
-
-  @include size-m {
-    nav {
-      display: flex;
-      background-color: #000000;
-      color: #FFFFFF;
-      border-radius: 0 40px 40px 0;
-      justify-content: center;
-      align-items: center;
-      height: 100%;
-
-      ul {
-        height: 35vh;
-        list-style: none;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-      }
-    }
+    display: flex;
   }
 </style>
 
 <nav>
-  <ul>
-    <List href="">Dashboard</List>
-    <List href="">Kaart</List>
-    <List href="">Alle meldingen</List>
-    <List href="">Instellingen</List>
-    <List href="">Uitloggen</List>
-  </ul>
+  <Hamburger bind:open={sidebar} />
 </nav>
