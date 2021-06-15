22ff02d0c26a7ae383a905c3a3e0c80cfda95d4c
veerleprins <veerleprins@hotmail.com>
2021-06-15 14:32:20 +0200

    Added segment & function on click / on key down

--- src/components/organisms/global/Header.svelte
+++ src/components/organisms/global/Header.svelte
@@ -1,14 +1,33 @@
 <script>
   // Components
+  import Hamburger from '@/components/atoms/Hamburger.svelte'
   import Nav from '@/components/molecules/Nav.svelte'
-  import Sidebar from '../../atoms/Sidebar.svelte'
+
+  // Props
+  export let segment
+
+  // Internals
   let open = false
+
+  /**
+   * This function checks whether a certain key is pressed
+   * or that it is clicked to change the toggle value.
+   * @param e
+   */
+  const handleNav = (e) => {
+    if (e.code === 'F1' || e.type === 'click') {
+      open = !open
+    }
+  }
 </script>
 
 <style lang="scss">
+  @import 'src/styles/index.scss';
 </style>
 
+<svelte:window on:keydown={handleNav} />
+
 <header>
-  <Sidebar bind:open />
-  <Nav bind:sidebar={open} />
+  <Hamburger {open} on:click={handleNav} />
+  <Nav {open} {segment} on:click={handleNav} />
 </header>
