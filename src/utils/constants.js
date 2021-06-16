80ccc61c6476d067588789e23decea2aa2d6cb92
veerleprins <veerleprins@hotmail.com>
2021-06-17 00:17:20 +0200

    Changed values

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
-export const width = 360
-export const height = 400
+export const width = 800
+export const height = 300
 export const margin = {
   top: 20,
-  right: 20,
-  bottom: 20,
-  left: 20,
+  right: 45,
+  bottom: 45,
+  left: 60,
 }
 export const innerHeight = height - margin.top - margin.bottom
 export const innerWidth = width - margin.left - margin.right
