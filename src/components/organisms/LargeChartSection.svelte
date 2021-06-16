bb0987ba9c46229a0f5dc2bcb429ea703b719946
veerleprins <veerleprins@hotmail.com>
2021-06-16 17:00:54 +0200

    Working twisted bar chart

--- src/components/organisms/LargeChartSection.svelte
+++ src/components/organisms/LargeChartSection.svelte
@@ -1,4 +1,40 @@
 <script>
+  // Externals
+  import { scaleBand, scaleLinear, max } from 'd3'
+
+  // Components
+  import Title from '@/components/atoms/Title.svelte'
+
+  // // Internals
+  import {
+    width,
+    height,
+    innerHeight,
+    innerWidth,
+    margin,
+  } from '../../utils/constants'
+  import { reportData } from '@/stores/dataStore.js'
+  import { getCleanedChartData } from '../../utils/cleaning/clean.js'
+
+  // Getting the data
+  let rawData
+  reportData.subscribe((value) => {
+    rawData = value
+  })
+  const data = getCleanedChartData(rawData)
+  console.log(data)
+
+  const yValue = (d) => d.type // time - tijd
+  const xValue = (d) => d.count // amount - aantal
+
+  const yScale = scaleBand()
+    .domain(data.map(yValue))
+    .range([0, innerHeight])
+    .padding(0.2)
+
+  const xScale = scaleLinear()
+    .domain([0, max(data, xValue)])
+    .range([0, innerWidth])
 </script>
 
 <style lang="scss">
@@ -9,7 +45,6 @@
     padding: 1em;
     background-color: $ui-section;
     border-radius: 15px;
-    box-shadow: 1px 1px 6px #9a9a9a;
     height: 100%;
   }
 
@@ -21,5 +56,26 @@
 </style>
 
 <section>
-  <h1>Insert fancy chart here</h1>
+  <Title isSubtitle>Aantal meldingen per uur</Title>
+  <svg {width} {height}>
+    <g transform={`translate(${margin.left},${margin.top})`}>
+      <g class="x-Axis">
+        <!-- xAs -->
+      </g>
+      <g class="y-Axis">
+        <!-- yAs -->
+      </g>
+      <g class="bars">
+        <!-- Bars -->
+        {#each data as d}
+          <rect
+            x={0}
+            y={yScale(yValue(d))}
+            width={xScale(xValue(d))}
+            height={yScale.bandwidth()}
+          />
+        {/each}
+      </g>
+    </g>
+  </svg>
 </section>
