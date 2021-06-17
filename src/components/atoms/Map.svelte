de30f463203b0394b6a2d66fc6091af657f61cde
johancvb <35685184+johancvb@users.noreply.github.com>
2021-06-17 16:06:47 +0200

    Changed style of map

--- src/components/atoms/Map.svelte
+++ src/components/atoms/Map.svelte
@@ -33,7 +33,6 @@
     ).addTo(mymap)
 
     let styling = {
-      color: 'gray',
       weight: 2,
       opacity: 1,
     }
@@ -52,7 +51,7 @@
 
           layer.setStyle({
             weight: 3,
-            color: '#666',
+
             dashArray: '',
             fillOpacity: 0.7,
           })
@@ -82,39 +81,6 @@
           onEachFeature: onEachFeature,
         }).addTo(mymap)
       })
-
-    function getColor(d) {
-      return d > 1000
-        ? '#800026'
-        : d > 500
-        ? '#BD0026'
-        : d > 200
-        ? '#E31A1C'
-        : d > 100
-        ? '#FC4E2A'
-        : d > 50
-        ? '#FD8D3C'
-        : d > 20
-        ? '#FEB24C'
-        : d > 10
-        ? '#FED976'
-        : '#FFEDA0'
-    }
-
-    function style(feature) {
-      return {
-        fillColor: getColor(feature.properties.density),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7,
-      }
-    }
-
-    let test = [{}]
-
-    L.geoJson(test, { style: style }).addTo(map)
   })
 </script>
 
