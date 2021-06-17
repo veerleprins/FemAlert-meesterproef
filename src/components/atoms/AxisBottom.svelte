1d2ccf848d8e7e7ab376b16510314db39b38c1ff
veerleprins <veerleprins@hotmail.com>
2021-06-17 08:53:52 +0200

    Working axis bottom

--- src/components/atoms/AxisBottom.svelte
+++ src/components/atoms/AxisBottom.svelte
@@ -2,27 +2,41 @@
   // Props
   export let xScale
   export let innerHeight
+  export let innerWidth
 </script>
 
 <style lang="scss">
   // Import fonts, vars, etc.
   @import 'src/styles/index.scss';
 
-  line {
-    stroke: $ui-black;
-  }
-
-  text {
-    fill: $ui-black;
+  g {
+    g {
+      line {
+        stroke: #c9c9c9;
+        text-anchor: middle;
+      }
+      text {
+        font-family: $light-font;
+        text-anchor: middle;
+        fill: black;
+      }
+    }
   }
 </style>
 
-{#each yScale.ticks() as tickValue}
-  <g key={tickValue} transform={`translate(${yScale(tickValue)}, 0)`}>
-    <line class="lines" y2={innerHeight} />
-    <text y={innerHeight + 3} dy=".71em" style={{ textAnchor: 'middle' }}>
-      <!-- {tickValue} -->
-      <slot />
-    </text>
+<g>
+  <g>
+    <line x1={0} x2={0} y1={0} y2={innerHeight} />
   </g>
-{/each}
+  {#each xScale.domain() as tickValue}
+    <g
+      transform={`translate(${xScale(tickValue) + xScale.bandwidth() / 2}, 0)`}
+    >
+      <line y1={0} y2={innerHeight} />
+      <text dy=".41em" y={innerHeight + 10}>{tickValue}</text>
+    </g>
+  {/each}
+  <g>
+    <line x1={innerWidth} x2={innerWidth} y1={0} y2={innerHeight} />
+  </g>
+</g>
